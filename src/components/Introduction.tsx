ede16917e8fc32df130d6e2893d66dac70498af2
farasat <farasatmateen@gmail.com>
2024-10-01 23:30:47 +0500

    my assignment

---
+++ src/components/Introduction.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Introduction() {
+  return (
+    <div className='text-5xl ml-[30px] md:ml-[80px] mt-[150px] font-serif text-red-500'>
+      Hello! <br/>
+      I am Farasat Mateen<br/>
+      <span className='text-3xl font-bold text-blue-600'>Electronic Engineer</span>
+      <p className='text-blue-300 text-base'>Talent Acquisition Lead 
+      <br/> Exide Pakistan Limited <br/>
+      </p>
+    </div>
+  )
+}
+
+export default Introduction;
